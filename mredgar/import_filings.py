c027e9c2d024b951ce9e0c092e3509902d4e639b
Friedrich Lindenberg <friedrich@pudo.org>
2014-10-11 14:35:52 +0200

    split into smaller pieces.

--- mredgar/import_filings.py
+++ mredgar/import_filings.py
@@ -12,6 +12,8 @@ log = logging.getLogger('importer')
 HOST = 'ftp.sec.gov'
 
 SIC_EXTRACT = re.compile(r'<ASSIGNED-SIC> *(.*)', re.I)
+DOC_IN = re.compile(r'^<DOCUMENT>\s*$', re.I)
+DOC_OUT = re.compile(r'^</DOCUMENT>\s*$', re.I)
 SICS = ['1311', '1381', '1382', '1389']
 
 
@@ -22,7 +24,41 @@ def match_sic(line):
         if sic in SICS:
             return True
     return False
+
+
+def split_filing(dir, line, file_name):
+    path = os.path.join(dir, file_name)
     
+    with open(path, 'r') as fh:
+        is_header = True
+        header = []
+        doc_idx = 0
+        doc = None
+        for tline in fh.readlines():
+            if doc is not None:
+                doc.append(tline)
+            
+            if DOC_IN.match(tline):
+                if is_header:
+                    is_header = False
+                    header = ''.join(header)
+                    if not match_sic(header):
+                        return
+                doc = [tline]
+            elif DOC_OUT.match(tline):
+                fn = '%s:%s:%s' % (line, file_name, doc_idx)
+                yield fn, {
+                    'header': header,
+                    'doc': ''.join(doc),
+                    'file_name': file_name,
+                    'doc_idx': doc_idx
+                }
+                doc = None
+                doc_idx += 1
+
+            if is_header:
+                header.append(tline)
+
 
 class MRImportFilings(MRJob):
 
@@ -44,13 +80,10 @@ class MRImportFilings(MRJob):
             tb = tarfile.open(tarpath, 'r:gz')
             tb.extractall(path=dir)
             for file_name in os.listdir(dir):
-                path = os.path.join(dir, file_name)
-                fn = '%s:%s' % (line, file_name)
-                with open(path, 'r') as fh:
-                    line = fh.read()
-                    if match_sic(line):
-                        yield fn, line
-
+                for res in split_filing(dir, line, file_name):
+                    #print res
+                    yield res
+                
         except EnvironmentError, ee:
             log.exception(ee)
         except Exception, e:
