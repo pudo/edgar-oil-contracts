a1a80b967c59a2e22ba66aabb1d39492e0f4cd85
Friedrich Lindenberg <friedrich@pudo.org>
2014-10-14 10:11:58 +0200

    compute scores

---
+++ mredgar/genscores.py
@@ -0,0 +1,24 @@
+import json
+import os
+import dataset
+
+DIR = 'scores'
+eng = dataset.connect('sqlite://')
+entries = eng['filings']
+
+for fn in os.listdir(DIR):
+    path = os.path.join(DIR, fn)
+    with open(path, 'r') as fh:
+        for line in fh.readlines():
+            key, value = line.split('\t', 1)
+            row = json.loads(value)
+            row['url'] = json.loads(key)
+            terms = row.pop('terms')
+            match_info = []
+            for term, weight in terms.items():
+                match_info.append('%s: %s' % (term, weight))
+            row['match_info'] = ', '.join(match_info)
+            entries.insert(row)
+
+dataset.freeze(entries, format='csv', filename='scored.csv')
+
